832a11eebd9f9dadf33fbfdd7657838fc17203a1
satyam mishra <satyam.growupnext@gmail.com>
2023-01-05 18:54:20 +0530

    title change

--- pages/services-pages/lowboy-shipping.php
+++ pages/services-pages/lowboy-shipping.php
@@ -7,11 +7,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Lowboy Trucking Transport | Shipping Services | +1-833-233-4447</title>
-    <meta name="title" content="Lowboy Trucking Transport | Shipping Services | +1-833-233-4447" />
+    <title>Lowboy Trucking Services |+1-833-233-4447</title>
+    <meta name="title" content="Lowboy Trucking Services |+1-833-233-4447" />
     <meta name="description" content="We provide the best Lowboy shipping services at an affordable price with our expertise transferred thousands of vehicles safely to their destination in the USA." />
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="Lowboy Trucking Transport | Shipping Services | +1-833-233-4447" />
+    <meta property="og:title" content="Lowboy Trucking Services |+1-833-233-4447" />
     <meta property="og:description" content="We provide the best Lowboy shipping services at an affordable price with our expertise transferred thousands of vehicles safely to their destination in the USA." />
     <meta property="og:url" content="https://rapidautoshipping.com/services/lowboy-shipping" />
     <meta property="og:site_name" content="lowboy Shipping - Rapid Auto Shipping" />
