7894eb919099d9777f2652ea61a3df997c5664b2
Vaibhav Tiwari <89778487+VaibhavTiwari00@users.noreply.github.com>
2023-01-19 18:37:18 +0530

    line haul transportation

--- pages/blogs/ship-from-new-york-to-florida.php
+++ pages/blogs/ship-from-new-york-to-florida.php
@@ -211,7 +211,7 @@
                     </li>
 
                     <div class="shipping-form-state-image">
-                        <img loading="lazy" src="<?= get_img() ?>bus-shipping-1.jpg" alt="Shipping Car To Florida" />
+                        <img loading="lazy" src="https://res.cloudinary.com/dsmsqwx6q/image/upload/…/Recent%20Uploads/truck-856345_960_720_cl2sil.jpg" alt="Shipping Car To Florida" />
 
                     </div>
                     <li id="7">
