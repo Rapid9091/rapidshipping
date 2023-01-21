6ba1454a28849f241a1edbfc80949e9bf105f62c
Vaibhav Tiwari <89778487+VaibhavTiwari00@users.noreply.github.com>
2023-01-21 13:31:58 +0530

    title update of alaska or hawaii pages

--- pages/services-pages/cutomized-container-shipping.php
+++ pages/services-pages/cutomized-container-shipping.php
@@ -43,16 +43,20 @@
     <meta property="article:tag" content="custom storage containers" />
     <meta property="article:tag" content="shipping container roll up doors" />
     <meta property="article:tag" content="custom containers" />
-
     <!--     Google Analytics Code Start -->
 
 
     <!--     Google Analytics Code Ends -->
     <?php include head; ?>
-    <link rel="stylesheet" href="<?= get_css(); ?>PAGE_blog.css">
+
+    <link rel="stylesheet" href="<?= get_css(); ?>PAGE_contactus.css">
+    <!-- animation css link   -->
+    <link rel="stylesheet" href="<?= get_css(); ?>small-form.css">
+    <link rel="stylesheet" href="<?= get_css(); ?>PAGE_colorado-to-arizona.css">
+    <link rel="stylesheet" href="<?= get_css(); ?>PAGE_why-choose-us.css">
+    <link rel="stylesheet" href="<?= get_css(); ?>side-form.css">
     <link rel="stylesheet" href="<?= get_css(); ?>PAGE_state-to-state-transport.css">
     <link rel="stylesheet" href="<?= get_css(); ?>PAGE_all-service-page.css">
-    <link rel="stylesheet" href="<?= get_css(); ?>PAGE_why-choose-us.css">
     <style>
         .contact-us-header-page {
             width: 100%;
@@ -69,74 +73,159 @@
 
 <body>
     <!-- this is header -->
-    <?php include_once header ?>
-    <!-- new section under the first section of single line -->
+    <?php include header; ?>
+    <!-- this is the fiorst section with background image -->
     <div class="contact-us-header-page">
         <?php
-        $NAME = " Customised shipping containers";
+        $NAME = "Customised shipping containers";
         $PARAGRAPH = "Rapid Auto Shipping can give you lots Customised shipping containers solutions to fit your needs. Domestic container movers charge different rates than international container carriers, but both have other things to consider. The dimensions and weight of your shipping container are shown here. The weight of the contents within a 40-foot shipping crate, for example, determines the cost of transport. We also can transport empty shipping containers.";
-        include small_form;
+        include_once small_form;
         ?>
     </div>
 
-    <section class="first-sub-service-page">
-        <h2 class="sub-service-heading-main">
-            CUSTOMIsED SHIPPING CONTAIMERS (RAS)
-        </h2>
-        <p>
-            Rapid Auto Shipping offers custom shipping containers solutions to meet your specific requirements. Prices for domestic container movers differ from rates for international container movers, but both have other considerations. This contains your shipping container's dimensions and weight. The cost of moving a 40-foot shipping container, for example, is determined by the weight of the cargo within. Empty shipping containers are also transported by us. Rapid Auto Shipping is one of the most reputable shipping container transportation firms in the business. We know how to deliver the best transportation options because we have over a decade of expertise.
-            <br><br>
-            When it comes to hauling containers, Rapid Auto Shipping saves you time and money. For your container delivery, a specialized transportation specialist determines the cheapest price. We don't accept just any truck or driver. We find the most cost-effective way to transport your shipping container. You require your shipping container to be delivered safely and on time. We've built our reputation on doing just that for over ten years. Rapid Auto Shipping is a shipping container relocation firm that takes care of everything. This covers things like paperwork, permits, and route planning, among other things. Get a price to transport a shipping container right now!
-            We also ship modified shipping containers.
-        </p>
-    </section>
-    <section class="first-sub-service-page">
-        <h2 class="sub-service-heading-main">
-            A RELIABLE SHIPPING CONTAINER MOVING COMPANY
-        </h2>
-        <p>
-            You Need Rapid Auto Shipping to Find a Reliable Shipping Container Moving Company When it comes to finding a dependable transport company to ship a container, you need Rapid Auto Shipping. We specialize in hauling big loads and know everything there is to know about it. For us, transporting cargo containers isn't just a sideline. It's what we do for a living. We will listen to your needs when you call us to relocate a container. Our shipping container movers will set a realistic timeline and determine the most effective route for your shipment. Any permissions, escorts, or pilot cars that are required will be provided by us. To keep your investment safe throughout travel, our moving team will load and secure your shipping container according to industry standards. Our professional movers can deliver your container to any place in the United States on time and in good condition. Cranes are frequently used to lift and load shipping containers on trucks, and we can help with that as well. Allow us to demonstrate how easy your next container relocation can be. For a free shipping container relocation quote, call immediately!
-        </p>
-    </section>
-    <section class="military-shipping-sixth-section">
-        <div class="military-shipping-sixth-section-info">
-            <h2 class="sub-service-heading-main"> SHIPPING CONTAINER MOVING COMPANIES: WHAT TO KNOW
-            </h2>
-            <p class="services-subtheory-lines-small">
-                It's a huge decision to hire a shipping container relocation firm. You should look for a business that is not only licensed, bonded, and insured, but also has experience carrying shipping containers. Having the proper loading and unloading equipment, such as cranes, is critical whether your container is full or empty. Rapid Auto Shipping is a one-stop-shop for relocating shipping containers. We assist with loading, handle all of the paperwork, and offer pilot cars and escort vehicles as needed. You need a container transportation business that cares, and we're here at Rapid Auto Shipping to make sure your shipping containers are delivered safely and on time. For a shipping container moving quote, give us a call right now!
-                We also ship modified shipping containers.
-            </p>
-        </div>
-        <div class="military-shipping-sixth-section-img">
-            <img src="<?= get_img() ?>background-bg-2.jpg" alt="customised container shipping" />
-        </div>
-    </section>
-    <section class="first-sub-service-page">
-        <h2 class="sub-service-heading-main">
-            DETERMINE THE BEST custom shipping containers TRANSPORT SERVICES
-        </h2>
-        <p>
-            You'll need to know the exact size and weight of the cargo you'll be shipping so you can be sure the item or things will fit in the container. Also, the type of cargo you're transporting has a big impact on the container style you'll require. For example, you must decide if you require transportation for a single large item or large quantities of tiny freight. Consider how your cargo will be loaded and unloaded into and out of the container. Some cargo must be loaded from the top, while others must be loaded from the side. Then consider whether temperature control is a concern for your cargo. If that's the case, refrigerated containers are the way to go.
-            We also ship modified shipping containers.
-        </p>
-    </section>
-    <section class="first-sub-service-page">
-        <h2 class="sub-service-heading-main">
-            HAULING SERVICES FOR custom shipping containers IN THEIR ENTIRETY
-        </h2>
-        <p>
-            That's correct! Heavy Haulers can transport your cargo container to any location in the United States or overseas, whether it is full or empty! With the best moving firm, you may ship heavy or empty 20', 40', or 45' containers. We provide movers for both local and cross-country relocation, including port to port, land to port, and port to land. We can help you with loads, permits, and more!
-            <br><br>
-            Cargo of various sizes and weights is transported using intermodal shipping containers. There are several types and sizes to choose from, each having its own shipping purpose. You must first determine the dimensions of your shipment before selecting a shipping container. Shipping container sizes follow the Industrial Standards Organization, or ISO, and are available in three lengths: 20 feet, 40 feet, and 45 feet. Cargo containers can be customised to fit the needs of international trade. There are a total of 12 distinct ISO containers to choose from.
-        </p>
-    </section>
+    <!-- parent grid section start-->
+    <div class="main-state-to-state">
+        <div class="left-state-to-state">
+            <div class="first-section-state-to-state">
+                <h2 class="heading-1">Customised Shipping Containers (RAS)</h2>
+                <p class="para-2">
+                    Rapid Auto Shipping offers custom shipping containers solutions to meet your specific requirements. Prices for domestic container movers differ from rates for international container movers, but both have other considerations. This contains your shipping container's dimensions and weight. The cost of moving a 40-foot shipping container, for example, is determined by the weight of the cargo within. Empty shipping containers are also transported by us. Rapid Auto Shipping is one of the most reputable shipping container transportation firms in the business. We know how to deliver the best transportation options because we have over a decade of expertise.
+                    <br><br>
+                    When it comes to hauling containers, Rapid Auto Shipping saves you time and money. For your container delivery, a specialized transportation specialist determines the cheapest price. We don't accept just any truck or driver. We find the most cost-effective way to transport your shipping container. You require your shipping container to be delivered safely and on time. We've built our reputation on doing just that for over ten years. Rapid Auto Shipping is a shipping container relocation firm that takes care of everything. This covers things like paperwork, permits, and route planning, among other things. Get a price to transport a shipping container right now!
+                    We also ship modified shipping containers.
+                </p>
+            </div>
+
+            <div class="points-div " id="top-section">
+                <h2 class="heading-2">
+                    What's in this article?
+                </h2>
+                <ul class="points-div-ul">
+                    <li><a href="#1">a reliable shipping container moving company</a></li>
+                    <li><a href="#2">shipping container moving companies: what to know</a></li>
+                    <li><a href="#3">determine the best custom shipping containers transport services</a></li>
+                    <li><a href="#4">hauling services for custom shipping containers in their entirety</a></li>
+
+                </ul>
+            </div>
+            <!-- section from which shipping started here -->
+            <section class="first-state-to-state">
+                <div class="shipping-form-state-image">
+                    <img loading="lazy" src="https://res.cloudinary.com/dsmsqwx6q/image/upload/v1674287987/Recent%20Uploads/cutomized-container-shipping-truck-01.jpg" alt="Customised Shipping Containers (RAS)" />
+
+                </div>
+                <ul>
+                    <li id="1">
+                        <div class="shipping-form-state-content">
+                            <h2 class="heading-1">Is <span>Open Auto Transport</span> A Cheapest Way To Ship A Car?</h2>
+                            <p class="para-2"> Open car shipping is by far the most cheapest way to ship a car. This will be confirmed by our car transportation calculator.
+                                <br>
+                                Because car transporters can fit more cars at a time, open transport car shipping is less expensive than enclosed auto transport. This means that a vehicle's expense is shared by a larger number of shippers, resulting in a lower cost. Furthermore, because the majority of car transporters are open carriers, there are more of them on the road, making booking considerably easier and faster. At Nationwide Auto Transportation, we do everything we can to assist carriers in filling their trucks, ensuring that our loyal customers receive the best available pricing.
+                            </p>
+
+                        </div>
+                    </li>
+                    <li id="2">
+                        <div class="shipping-form-state-content">
+                            <h2 class="heading-1">What Is The Distinction Between <span>Open</span> And <span>Enclosed</span> Trailers?</h2>
+
+                            <p class="para-2">
+                                Open carrier and enclosed carrier are the two basic types of auto shipment.
+                                <br>
+                                When you ship your car on an open carrier, it is known as open carrier car transport. This is the most cost-effective auto transportation option available, as opposed to an enclosed carrier. Your vehicle, however, will be exposed to the elements, such as rain, hail, and snow. External influences, in fact, have the potential to harm your vehicle. However, this is a remote possibility that occurs only infrequently.
+                            </p>
 
 
+                        </div>
+                    </li>
+                    <div class="shipping-form-state-image">
+                        <img loading="lazy" src="https://res.cloudinary.com/dsmsqwx6q/image/upload/v1674288064/Recent%20Uploads/cutomized-container-shipping-truck-02.jpg" alt="Customised Shipping Containers" />
+                    </div>
+                    <li id="3">
+                        <div class="shipping-form-state-content">
+                            <h2 class="heading-1">What Is The Cost Of <span>Open Auto Transport</span>?</h2>
+                            <p class="para-2"> The cost of open car transport varies depending on your specific needs at <strong><a href="<?= home_path() ?>">Rapid Auto Shipping</a></strong>. The cost of open automobile shipment is determined by a number of factors. These factors include your car's type and model, the overall distance of your shipping, and whether or not your vehicle is in good working order.
+                                <br><br>
+                                When you're ready, our instant open car transport cost calculator can quickly provide you with open car shipping quotes. You can even keep your identity hidden! Simply provide us with some basic information about your vehicle and shipment route. You'll have exact open automobile shipping quotes to select from in a matter of seconds.
+                            </p>
+                        </div>
+                    </li>
+
+                    <li id="4">
+                        <div class="shipping-form-state-content">
+                            <h2 class="heading-1">What Are The Benefits Of Using An Open Auto Carrier?</h2>
+                            <p class="para-2">
+                                Open carriers are becoming the most popular method of cost-effective car shipping. As a result, auto transport service companies are in a fierce rivalry. As a result of this competition, auto shipping companies are compelled to do everything possible to deliver high-quality services at a cheaper cost. Because the vehicle transportation industry is booming, serious companies must give the best services available to stay competitive.
+
+                                Another big benefit for customers is that vehicle shipping companies can accommodate more vehicles on their trucks, lowering car shipping costs significantly and further lowering prices.
+                                <br><br>
+                                The conventional ten-vehicle transporter is the most typical open auto carrier, which is frequently used to transport new vehicles to dealerships. It is the most cost-effective method of transporting multiple autos in one trip. Both service providers and clients profit from it. Carriers make the most money, while customers get the best auto transportation deals.
+                                <br><br>
+                                Customers on a budget aren't the only ones that choose open carrier auto transport. It is also used by most dealerships, collectors, and manufacturers. The cost of transporting ten autos at once has a substantial impact on the total cost of auto shipping. Open carriers of various sizes that can transport fewer vehicles are also available, although ten-car carriers are the most frequent.
+                                <br><br>
+                                Unless you need to avoid potential damage while carrying a highly fragile and expensive car or a car that you rarely use, open automobile carriers are the best option for you.
+                            </p>
+
+                        </div>
+                    </li>
+
+
+
+
+                </ul>
+
+                <div class="shipping-form-state-image">
+                    <img loading="lazy" src="https://res.cloudinary.com/dsmsqwx6q/image/upload/v1674287912/Recent%20Uploads/cutomized-container-shipping-truck.jpg" alt="Customised Shipping Containers" />
+                </div>
+            </section>
+
+
+        </div>
+
+        <div class="right-state-to-state-child" id="right-state-to-state-child">
+            <?php
+            include side_form;
+            ?>
+        </div>
+    </div>
+
+
+
+    <!-- top scroll button start -->
+    <div class="top-scroll-btn-relative " id="top-btn">
+        <a href="#top-section">
+            <div class="top-scroll-btn-fixed">
+                <i class="fa-solid fa-arrow-down"></i>
+            </div>
+        </a>
+
+    </div>
+
+    <!-- top scroll button end -->
+
+    <!-- parent grid section end  -->
     <?php include_once why_choose_us_section ?>
 
     <?php include_once call_to_action ?>
 
     <?php include_once page_bottom ?>
+
+
+    <script>
+        window.onscroll = function() {
+
+            scrollTop()
+        };
+        var mybutton = document.getElementById("top-btn");
+
+        function scrollTop() {
+            if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+    </script>
 </body>
 
 </html>
