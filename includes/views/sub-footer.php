e02f95c8e30ed3b1bd1864e5fa16e01b8bdac193
Faiz <mohdfaiz.1842@gmail.com>
2023-03-23 17:33:03 +0530

    chnage yt link

--- includes/views/sub-footer.php
+++ includes/views/sub-footer.php
@@ -56,7 +56,7 @@
                             <a href=" https://www.linkedin.com/in/rapid-auto-shipping-ba4593232/" target="_blank"><img loading="lazy" height="30" width="30" src="<?= get_img() ?>linkedin-icon.png" alt="rapid auto shipping links"></a>
                         </div>
                         <div class="icon-holder-grid-1">
-                            <a href="https://www.youtube.com/" target="_blank"><img loading="lazy" height="30" width="30" src="<?= get_img() ?>yt.png" alt="rapid auto shipping links"></a>
+                            <a href="https://www.youtube.com/@rapidautoshipping7218" target="_blank"><img loading="lazy" height="30" width="30" src="<?= get_img() ?>yt.png" alt="rapid auto shipping links"></a>
                         </div>
 
                     </div>
