6e78b46c0387d14575cf9488653e64f1141fa058
Faiz <mohdfaiz.1842@gmail.com>
2023-03-28 12:13:34 +0530

    wifg

--- includes/views/strip-under-header.php
+++ includes/views/strip-under-header.php
@@ -1,23 +1,22 @@
 <section class="review-bar-under-header">
     <div class="review-bar-under-header-grid">
         <div class="review-bar-under-header-grid-1">
-            <div class="review-bar-under-header-grid-1-image">
                 <img loading="lazy" src="<?= get_img() ?>lorry-icon.png" alt="review rapid auto shipping" />
-            </div>
+        
             <h2>2 million +</h2>
             <p>Successful Delivery</p>
         </div>
         <div class="review-bar-under-header-grid-1">
-            <div class="review-bar-under-header-grid-1-image">
+           
                 <img loading="lazy" src="<?= get_img() ?>globe-icon.png" alt="review rapid auto shipping" />
             </div>
             <h2>190+</h2>
             <p>Countries Served</p>
-        </div>
+        
         <div class="review-bar-under-header-grid-1">
-            <div class="review-bar-under-header-grid-1-image">
+            
                 <img loading="lazy" src="<?= get_img() ?>happy-customer-icon.png" alt="review rapid auto shipping" />
-            </div>
+          
             <h2>99% +</h2>
             <p>Happy Customers</p>
         </div>
