c412647b6c437a85eb174a092f52f5c5a6ee451c
sachin yadav <sy396610@gmail.com>
2023-03-29 12:49:41 +0530

    change

--- pages/blogs/fifth-wheel-transport-services.php
+++ pages/blogs/fifth-wheel-transport-services.php
@@ -156,7 +156,7 @@
                     </p>
                     <img loading="lazy" style="width:100%"
                         src="https://res.cloudinary.com/dsmsqwx6q/image/upload/v1667900696/Recent%20Uploads/verification_qtasyq.jpg"
-                        alt="Best Dallas auto transport company">
+                        alt="Best fifth-wheel-transport-services">
 
                 </div>
             </div>
