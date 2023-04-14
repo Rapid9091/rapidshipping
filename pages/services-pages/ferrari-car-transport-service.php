5a337c990db726b18ace418f1cdb23cbdf068d90
sachin yadav <sy396610@gmail.com>
2023-04-14 16:23:42 +0530

    chnages

--- pages/services-pages/ferrari-car-transport-service.php
+++ pages/services-pages/ferrari-car-transport-service.php
@@ -17,11 +17,7 @@
 <meta property="og:url" content="https://rapidautoshipping.com/services/ferrari-car-transport-service" />
 <meta property="og:site_name" content="Best Ferrari Car Transport Services | +1-833-233-4447" />
 <meta name='robots' content='index' />
-<<<<<<< HEAD
 <meta name="keywords" content=" car transport service, car shipping,Best Ferrari Car Transport Services, car shipping cost, auto transport , vehicle transport services" />
-=======
-<meta name="keywords" content="car transport service, car shipping, Best Ferrari Car Transport Services, car shipping cost, auto transport , vehicle transport services" />
->>>>>>> 5d25f6b33bef5a1bb91b885e76ca6f992b48f20b
 <meta property="article:tag" content="car transport service" />
 <meta property="article:tag" content="car shipping" />
 <meta property="article:tag" content="Best Ferrari Car Transport Services" />
@@ -160,11 +156,7 @@
                     </li>
                     <div class="shipping-form-state-image">
                             <img loading="lazy" src="<?=get_img();?>ferrari-2.webp"
-<<<<<<< HEAD
                                 alt=" car shipping" />
-=======
-                                alt="car shipping" />
->>>>>>> 5d25f6b33bef5a1bb91b885e76ca6f992b48f20b
 
                         </div>
                     <li id="3">
