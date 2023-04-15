bb4b51888ee9557491d83bea3169f858eb441ae0
Zafar <husain.zafar13@gmail.com>
2023-04-15 15:32:00 +0530

    .

--- pages/states/_index.php
+++ pages/states/_index.php
@@ -25,7 +25,7 @@ if (isset($_GET['page_id'])) {
     <?= $data['meta']; ?> 
     <?php include_once new_head; ?>
     <!-- <link rel="stylesheet" href="<?= get_css(); ?>Page_state_new.min.css"> -->
-    <link rel="stylesheet" href="<?= get_css(); ?>Page_state_new.css">
+    <link rel="stylesheet" href="<?= get_css(); ?>Page_state_new.min.css">
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet" defer/>
 
