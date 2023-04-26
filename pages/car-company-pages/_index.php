938816a5d6d774cb5665c2d35fe426f6b44355eb
Faiz <mohdfaiz.1842@gmail.com>
2023-04-26 17:15:43 +0530

    remove\

--- pages/car-company-pages/_index.php
+++ pages/car-company-pages/_index.php
@@ -21,12 +21,16 @@ if (isset($_GET['page_id'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $data['title'];?></title>
-    <?= $data['meta']; ?> 
+    <title>
+        <?= $data['title'];?>
+    </title>
+    <?= $data['meta']; ?>
     <?php include_once new_head; ?>
     <link rel="stylesheet" href="<?= get_css(); ?>Page_new.min.css" defer>
 
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet" defer/>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400&display=swap"
+        rel="stylesheet" defer />
 
     <!-- ------------------------------ -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
@@ -36,10 +40,9 @@ if (isset($_GET['page_id'])) {
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
         integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" defer />
-    
+
 
     <style>
-        
         .contact-us-header-page {
             width: 100%;
             background-repeat: no-repeat, repeat;
@@ -50,8 +53,11 @@ if (isset($_GET['page_id'])) {
             background-repeat: no-repeat;
             background-size: cover;
         }
-
-        
+        .third-section-content{
+            display: flex;
+            justify-content: space-between;
+            flex-direction: column;
+        }
     </style>
 </head>
 
@@ -68,39 +74,44 @@ if (isset($_GET['page_id'])) {
 
 
             <div class="third-section-content">
+                <div class="state-banner-content">
+                    <h1 class="third-section-content-heading">
 
-                <h1 class="third-section-content-heading">
+                        <?php echo $data['h1']; ?>
+                    </h1>
 
-                    <?php echo $data['h1']; ?>
-                </h1>
-
-                <p class="third-section-content-text" style=" overflow: hidden;
+                    <p class="third-section-content-text" style=" overflow: hidden;
                 text-overflow: ellipsis;
    display: -webkit-box;
    -webkit-line-clamp: 5;
            line-clamp: 5; 
    -webkit-box-orient: vertical;
    padding:1% 0">
-                    <?php echo $data['h1_about']; ?>
+                        <?php echo $data['h1_about']; ?>
 
-                </p>
-
-                <div class="banner-review-images">
-                <img src="<?=get_img()?>rapid-auto-shipping-trustpilot-faiz.webp" alt="trustpilot logo" loading="lazy" width="150px" height="71px" class="truspilot_logo">
-                <img src="<?=get_img()?>rapid-auto-shipping-trustpilot-google.webp" alt=" google logo" loading="lazy" width="150px" height="66px" class="google_logo">
-                <img src="<?=get_img()?>rapid-auto-shipping-trustpilot-crunchbase2.webp" alt="cruchbase logo" loading="lazy" width="150px" height="44px" class="crunchbase_logo">
+                    </p>
+                    </div>
+                    <div class="state-banner-images">
+                        <div class="banner-review-images">
+                            <img src="<?=get_img()?>rapid-auto-shipping-trustpilot-faiz.webp" alt="trustpilot logo"
+                                loading="lazy" width="150px" height="71px" class="truspilot_logo">
+                            <img src="<?=get_img()?>rapid-auto-shipping-trustpilot-google.webp" alt=" google logo"
+                                loading="lazy" width="150px" height="66px" class="google_logo">
+                            <img src="<?=get_img()?>rapid-auto-shipping-trustpilot-crunchbase2.webp"
+                                alt="cruchbase logo" loading="lazy" width="150px" height="44px" class="crunchbase_logo">
+                        </div>
                     </div>
 
 
-            </div>
-            <div id="banner_form_small" style="display:grid;
+                </div>
+                <div id="banner_form_small" style="display:grid;
                 place-items:center;">
-                <?php include small_form_new ;?>
+                    <?php include small_form_new ;?>
+                </div>
+
+
+
             </div>
-
-
-
-        </div>
     </section>
     <hr>
     <!-------------feature strip ----------------->
@@ -108,7 +119,8 @@ if (isset($_GET['page_id'])) {
         <div class="main flex">
             <div class="first flex">
                 <div class="first-1 strip_logo">
-                    <img src="<?=get_img()?>state-to-state/dollar-symbol.webp" alt=" dollar" loading="lazy" width="50px" height="50px">
+                    <img src="<?=get_img()?>state-to-state/dollar-symbol.webp" alt=" dollar" loading="lazy" width="50px"
+                        height="50px">
                 </div>
                 <div class="first-2">
                     <div class="heading">
@@ -122,7 +134,8 @@ if (isset($_GET['page_id'])) {
             </div>
             <div class="second flex">
                 <div class=" second-1 strip_logo">
-                    <img src="<?=get_img()?>state-to-state/calendar.png" alt="calendar" loading="lazy" width="50px" height="50px">
+                    <img src="<?=get_img()?>state-to-state/calendar.png" alt="calendar" loading="lazy" width="50px"
+                        height="50px">
                 </div>
                 <div class="second-2">
                     <div class="heading">
@@ -136,7 +149,8 @@ if (isset($_GET['page_id'])) {
             </div>
             <div class="third flex">
                 <div class="third-1 strip_logo">
-                    <img src="<?=get_img()?>state-to-state/distance.webp" alt="distance" loading="lazy" width="50px" height="50px">
+                    <img src="<?=get_img()?>state-to-state/distance.webp" alt="distance" loading="lazy" width="50px"
+                        height="50px">
                 </div>
                 <div class="third-2">
                     <div class="heading">
@@ -151,7 +165,8 @@ if (isset($_GET['page_id'])) {
             </div>
             <div class="forth flex">
                 <div class="fourt-1 strip_logo">
-                    <img src="<?=get_img()?>state-to-state/shield.webp" alt="sheild" loading="lazy" width="50px" height="50px">
+                    <img src="<?=get_img()?>state-to-state/shield.webp" alt="sheild" loading="lazy" width="50px"
+                        height="50px">
                 </div>
                 <div class="forth-2">
                     <div class="heading">
@@ -176,7 +191,7 @@ if (isset($_GET['page_id'])) {
         <div class="left-state-to-state">
             <div class="first-section-state-to-state">
                 <h2 class="heading-1"><strong>
-                    <?=$data['h2'];?>
+                        <?=$data['h2'];?>
                     </strong></h2>
                 <p class="para-2">
                     <?= $data['h2_about'];?>
@@ -188,7 +203,7 @@ if (isset($_GET['page_id'])) {
             <section class="first-state-to-state">
                 <div class="main_content_div">
                     <?php echo $data['content'];?>
-                 </div>
+                </div>
 
             </section>
 
@@ -229,24 +244,28 @@ if (isset($_GET['page_id'])) {
             <h2 class="heading-1">Advantages using <span>Rapid Auto Shipping</span></h2>
             <div class="rapid-advantages-grid utility_grid utility_grid-4">
                 <div class="rapid-advantages-grid-1 ">
-                    <img src="<?= get_img() ?>icons/rating.webp" alt="best car shipping in USA" loading="lazy" width="80px" height="80px">
+                    <img src="<?= get_img() ?>icons/rating.webp" alt="best car shipping in USA" loading="lazy"
+                        width="80px" height="80px">
                     <h3>Top Rated Auto Shipping Company</h3>
                     <p>We have thousands of 5-star reviews for exceptional vehicle shipping services.</p>
                 </div>
 
                 <div class="rapid-advantages-grid-1 ">
-                    <img src="<?= get_img() ?>icons/auto-insurance-icon.webp" alt="best car shipping in USA" loading="lazy" width="80px" height="80px">
+                    <img src="<?= get_img() ?>icons/auto-insurance-icon.webp" alt="best car shipping in USA"
+                        loading="lazy" width="80px" height="80px">
                     <h3>Complete Vehicle Coverage</h3>
                     <p>We offer the most comprehensive coverage options for complete peace of mind.</p>
                 </div>
                 <div class="rapid-advantages-grid-1 ">
-                    <img src="<?= get_img() ?>icons/savings-icon.webp" alt="best car shipping in USA" loading="lazy" width="80px" height="80px">
+                    <img src="<?= get_img() ?>icons/savings-icon.webp" alt="best car shipping in USA" loading="lazy"
+                        width="80px" height="80px">
                     <h3>No Up-Front Deposit Required</h3>
                     <p>You don't pay a penny until the day your shipment is scheduled for pickup.</p>
                 </div>
 
                 <div class="rapid-advantages-grid-1 ">
-                    <img src="<?= get_img() ?>icons/online-tracking.webp" alt="Albama to alaska shipping" loading="lazy" width="80px" height="80px">
+                    <img src="<?= get_img() ?>icons/online-tracking.webp" alt="Albama to alaska shipping" loading="lazy"
+                        width="80px" height="80px">
                     <h3>24/7 Online Shipment Tracking</h3>
                     <p>You can track the status of your shipment online anytime at your convenience.</p>
                 </div>
@@ -266,8 +285,12 @@ if (isset($_GET['page_id'])) {
                 $car_model = ucfirst($car_model_array[0]);
 
                 ?>
-                Some famous <span><?= $car_model;?></span> Cars Models that we usually ship</h2>
-            <div class="" style="display:grid; grid-template-columns: repeat(auto-fit, minmax(150px, 1fr)); padding:2% 5%; text-align:center;">
+                Some famous <span>
+                    <?= $car_model;?>
+                </span> Cars Models that we usually ship
+            </h2>
+            <div class=""
+                style="display:grid; grid-template-columns: repeat(auto-fit, minmax(150px, 1fr)); padding:2% 5%; text-align:center;">
                 <?php
                   $json = $data['car_models'];
                   $json_decoded = json_decode($json);
@@ -279,10 +302,10 @@ if (isset($_GET['page_id'])) {
                         }
                     }
                     ?>
-                
-                
 
-                </div>
+
+
+            </div>
         </section>
     </section>
 
@@ -380,38 +403,59 @@ if (isset($_GET['page_id'])) {
         <div class="faq">FACTS & QUESTIONS</div>
 
         <div class="container">
-            
+
             <div class="box">
                 <span>What Kind of Paperwork Will I Need?</span>
                 <i class='bx bx-plus'></i>
             </div>
-            <p class="para-faq">When transporting automobiles from one state to another, a bill of lading - or, as previously stated, a vehicle health report - is required. Every car transporter carries one with them, either online or on paper, for each vehicle they move. You will also receive a duplicate.
-            <br>Provide no ownership or certificate of sale. The deal with the car transport provider will say that you are authorizing them to send the car whether or not the owner is in your name. When purchasing from a sale or a vendor, keep the title and car transport services distinct. You don't want papers to get lost or overlooked, so have the title shipped straight to you!</p>
+            <p class="para-faq">When transporting automobiles from one state to another, a bill of lading - or, as
+                previously stated, a vehicle health report - is required. Every car transporter carries one with them,
+                either online or on paper, for each vehicle they move. You will also receive a duplicate.
+                <br>Provide no ownership or certificate of sale. The deal with the car transport provider will say that
+                you are authorizing them to send the car whether or not the owner is in your name. When purchasing from
+                a sale or a vendor, keep the title and car transport services distinct. You don't want papers to get
+                lost or overlooked, so have the title shipped straight to you!
+            </p>
             <div class="box">
                 <span>Can I put personal items in the car?</span>
                 <i class='bx bx-plus'></i>
             </div>
-            <p class="para-faq">Yes, but only up to 100 pounds at a time and at your peril. Visit our help subject for shipping a vehicle with belongings to learn more.</p>
+            <p class="para-faq">Yes, but only up to 100 pounds at a time and at your peril. Visit our help subject for
+                shipping a vehicle with belongings to learn more.</p>
             <div class="box">
                 <span>What Kind of Tracking Services Do You Offer?</span>
                 <i class='bx bx-plus'></i>
             </div>
-            <p class="para-faq">We offer a standard monitoring device to keep track of your car while it is in transit. You can monitor your shipment using the online tracking function 24/7. It's all part of our dedication to our clients. To monitor your vehicle shipment, please provide us with your tracking information, name, email address, and/or phone number.</p>
+            <p class="para-faq">We offer a standard monitoring device to keep track of your car while it is in transit.
+                You can monitor your shipment using the online tracking function 24/7. It's all part of our dedication
+                to our clients. To monitor your vehicle shipment, please provide us with your tracking information,
+                name, email address, and/or phone number.</p>
             <div class="box">
                 <span>Is My Pick-Up or Delivery Date Guaranteed?</span>
                 <i class='bx bx-plus'></i>
             </div>
-            <p class="para-faq">Guaranteed pick-up is the best choice for those who want to prepare ahead of time and ensure that their vehicle arrives on time. The service is not inexpensive because it is tailored to your specific requirements.<br> You can choose the precise day your vehicle will be picked up with this service, which can save you time and money. Furthermore, you will have a piece of mind knowing that everything will go easily with your relocation.</p>
+            <p class="para-faq">Guaranteed pick-up is the best choice for those who want to prepare ahead of time and
+                ensure that their vehicle arrives on time. The service is not inexpensive because it is tailored to your
+                specific requirements.<br> You can choose the precise day your vehicle will be picked up with this
+                service, which can save you time and money. Furthermore, you will have a piece of mind knowing that
+                everything will go easily with your relocation.</p>
             <div class="box">
                 <span>How Much Should I Expect to Spend on Shipping My Car?</span>
                 <i class='bx bx-plus'></i>
             </div>
-            <p class="para-faq">The expense of car transport to or from Alabama is highly influenced by the sort of vehicle, the distance traveled, and the technique used. <br>Transporting a car from a greater distance costs more. Longer distances, on the other hand, may result in fewer average kilometres per dollar than shorter ones. <br>For a precise vehicle shipment price for Alabama, use our free instant car shipping estimate calculator above.</p>
+            <p class="para-faq">The expense of car transport to or from Alabama is highly influenced by the sort of
+                vehicle, the distance traveled, and the technique used. <br>Transporting a car from a greater distance
+                costs more. Longer distances, on the other hand, may result in fewer average kilometres per dollar than
+                shorter ones. <br>For a precise vehicle shipment price for Alabama, use our free instant car shipping
+                estimate calculator above.</p>
             <div class="box">
                 <span>Should I Expect Any Extra Charges Once My Car is Delivered?</span>
                 <i class='bx bx-plus'></i>
             </div>
-            <p class="para-faq">The price is significantly influenced by the distance your vehicle is shipped. If you are shipping your vehicle from or to a remote place, you may be charged additional costs. Auto carriers, for example, may charge an extra fee to drive your vehicle to a less crowded area because it requires more time and is usually much farther away from the carrier's next location.</p>
+            <p class="para-faq">The price is significantly influenced by the distance your vehicle is shipped. If you
+                are shipping your vehicle from or to a remote place, you may be charged additional costs. Auto carriers,
+                for example, may charge an extra fee to drive your vehicle to a less crowded area because it requires
+                more time and is usually much farther away from the carrier's next location.</p>
 
 
 
@@ -423,7 +467,7 @@ if (isset($_GET['page_id'])) {
         <div class="b_top">
             <i class='bx bxs-phone' style="font-size: 20px;"></i>
             <a href="tel:+1-833-233-4447">+1-833-233-4447</a>
-        
+
         </div>
     </div>
 
