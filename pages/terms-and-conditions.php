ad375a265c4ebf29f14971db2df6065b417e9b64
Faiz <mohdfaiz.1842@gmail.com>
2023-05-13 18:35:17 +0530

    add page

---
+++ pages/terms-and-conditions.php
@@ -0,0 +1,143 @@
+<?php include_once "../init.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terms & Conditions</title>
+    <meta property="og:description" content="terms and conditions about rapid auto shipping" />
+    <?php include head;    ?>
+    <link rel="stylesheet" href="<?= get_css(); ?>car-key-section.css">
+    <style>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+  body{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+.policy-container{
+    padding: 50px;
+    background-color: #80808021;
+    font-family: "Poppins", sans-serif;
+}
+.policy-container h1{
+    text-align: center;
+}
+.policy-container h2{
+    margin-top: 10px;
+}
+.policy-container p{
+    font-weight: 400;
+}
+    </style>
+</head>
+<body>
+<?php include header; ?>
+    <div class="policy-container">
+
+
+        <h1>Terms and Conditions</h1>
+        <p>Welcome to Rapid Auto Shipping!</p>
+        <p>These terms and conditions explain how to use the https://rapidautoshipping.com website, which is owned by Flampeg Solutions LLC.</p>
+        <p>If you visit this website, we'll assume you agree to these rules. If you don't agree to all of the terms and conditions on this page, don't use Rapid Auto Shipping anymore.</p>
+
+
+
+
+        <h2>Cookies:</h2>
+        <p>Cookies are used to help personalize your experience on the website. You agreed to use the needed cookies when you went to Rapid Auto Shipping.</p>
+        <p>A cookie is a text file that a web page service puts on your hard drive. Cookies can't be used to run programs on your machine or send viruses to it. Cookies are made just for you, and only a web server with the same name as the one that gave you the cookie can read them.</p>
+        <p>To run our website, we may use cookies to gather, store, and track information for statistical or marketing reasons. You can choose whether to accept extra cookies or not. There are some cookies on our website that are needed for it to work. These cookies always work, so you don't have to agree to them. Please keep in mind that if you accept the required cookies, you are also accepting cookies from third parties. These cookies could be used by third-party services if you use them on our website, like a video show window that was made by a third party and added to our website.</p>
+
+
+
+        <h2>Services :</h2>
+        <ul>
+            <li>Upon the customer's request, Flampeg Solutions LLC shall arrange for the transportation of the customer's shipment by carriers in accordance with these terms. Flampeg Solutions LLC, at its sole discretion, retains the right to refuse or cancel any order at any time.</li>
+            <li>When a carrier accepts the customer's order, Flampeg Solutions LLC's services are considered finished.</li>
+            <li>The customer acknowledges and agrees that Flampeg Solutions LLC (i) solely acts as a transportation broker, (ii) is not a motor carrier or transporter, and (iii) does not represent itself as providing property transportation.</li>
+            <li>Flampeg Solutions LLC fully acknowledges and agrees that it never takes custody or ownership of, transports, or handles the customer's shipment, nor does it assume any obligation for the shipment.</li>
+            <li>Flampeg Solutions LLC will be subject to the terms and conditions of the ocean carrier's bill of lading, tariff, schedules, rates, and rules for all ocean transportation arranged. Ocean carriers provide limited liability coverage for loss or damage, and the customer must ask about acquiring additional insurance with the ocean carrier.</li>
+            <li>Flampeg Solutions LLC will provide an estimated pickup and delivery date to the customer; however, delays may occur prior to and/or during transport due to weather or road conditions, government regulations, mechanical problems, and other causes beyond Flampeg Solutions LLC's control. Flampeg Solutions LLC cannot and will not make any guarantees about delivery dates or timeframes.</li>
+            <li>The customer understands and acknowledges that Flampeg Solutions LLC is not responsible or liable for any damages or expenses incurred as a result of any type of delay or for any reason.</li>
+        </ul>
+        
+
+
+
+        <h2>License:</h2>
+        <p>Discover the exclusive intellectual property rights to all content on Rapid Auto Shipping, which are owned by Flampeg Solutions LLC and/or its licensors, unless explicitly stated otherwise. Experience the full protection of our intellectual property rights. All rights reserved. Gain access to this exclusive service offered by Rapid Auto Shipping for your personal use while adhering to the terms and conditions outlined with restrictions.</p>
+        <p>You must not:</p>
+        <ul>
+            <li>Copy or republish material from Rapid Auto Shipping</li>
+            <li>Sell, rent, or sub-license material from Rapid Auto Shipping</li>
+            <li>Reproduce, duplicate, or copy material from Rapid Auto Shipping</li>
+            <li>Redistribute content from Rapid Auto Shipping</li>
+        </ul>
+        <p>This agreement will commence on the current date.</p>
+        <p>
+        Certain sections of this website provide users with the chance to share and discuss opinions and information within specific areas of the site. Flampeg Solutions LLC does not engage in the filtering, editing, publishing, or reviewing of comments prior to their appearance on the website. The views and opinions expressed in comments are not representative of those held by Flampeg Solutions LLC, its agents, and/or affiliates. Comments express the perspectives and beliefs of the individual who shares them. Flampeg Solutions LLC is not liable for any damages, expenses, or liability caused by the comments on this website, to the extent permitted by applicable laws. This includes any use, posting, or appearance of the comments.
+        </p>
+        <p>Flampeg Solutions LLC retains the authority to observe all comments and eliminate any comments that may be deemed unsuitable, offensive, or violate these Terms and Conditions.</p>
+        <p>You warrant and represent that:</p>
+        <ul>
+            <li>You have the right to post comments on our website and have obtained all necessary licenses and consents to do so.</li>
+            <li>The comments are free from any infringement of intellectual property rights, such as copyright, patent, or trademark, belonging to any third party.</li>
+            <li>The user agrees to refrain from posting any comments that may be considered defamatory, libelous, offensive, indecent, or unlawful and that may infringe on the privacy of others.</li>
+            <li>Comments should not be utilized to solicit or promote business or engage in commercial activities or illegal practices.</li>
+            <p>By agreeing to these terms, you are giving Flampeg Solutions LLC permission to use your comments in any form, format, or medium. This includes the right to reproduce, edit, and authorize others to use and edit your comments. The license granted to Flampeg Solutions LLC is non-exclusive.</p>
+        </ul>
+
+
+        <h2>Customer’s Responsibilities</h2>
+        
+        <ul>
+            <li><b>The accuracy of information.</b> The customer is solely responsible for ensuring the accuracy of all details in the order confirmation and on the carrier's form, bill of lading, or other required documents, including the shipment's description (year, make, model, body style, trim, etc.), point of origin, destination, fees, and special instructions. The customer acknowledges and accepts this responsibility. Making alterations or corrections to the shipment description or any other aspect of an order may lead to extra charges or the cancellation of the order. The customer agrees to waive any claims against Flampeg Solutions LLC for cancellation or additional charges in case the shipment received does not match the shipment listed in the order confirmation.</li>
+            <li><b>Shipment size and condition.</b> Customers are required to notify Flampeg Solutions LLC about the size and condition of their shipment when making a booking and before the scheduled pick-up date. The customer acknowledges and agrees that if the shipment becomes inoperable during transit, or if the shipment has been modified from its original equipment manufacturer ("OEM") condition with features such as aftermarket spoilers, lowered chassis, height modifications, etc., or if the shipment is oversized due to dual or oversized wheels, extra-large tires, racks, a lifted chassis, or if it is a large vehicle such as a limousine or a hearse, then the carrier may impose additional fees to transport the shipment or decline to transport it all together.</li>
+            <li><b>The shipment is currently being prepared.</b> The customer acknowledges and agrees that they are responsible for preparing the shipment for transportation. Customers must secure or remove all loose parts, fragile attachments, and low-hanging spoilers, among other things, prior to shipment. Prior to shipment, customers are required to remove all non-permanent luggage and other racks that are mounted outside. The shipment should be handed over to the carrier in a functional state, and the fuel level should not exceed one-fourth of the tank's capacity. The customer acknowledges and agrees that they are accountable for any damages, losses, or claims to the shipment, other vehicles, and/or individuals that may result from any part of the shipment becoming loose or detached during transportation.</li>
+            <li><b>Alarm.</b> Customers are required to either disarm any alarm system that has been installed in the shipment or provide the carrier with clear instructions on how to disengage it. In the event that an alarm is triggered while in transit and there are no keys or instructions provided to turn it off, the carrier may need to use any reasonable means available to silence the alarm. The customer will not have any recourse in this situation.</li>
+            <li><b>Personal Property.</b>  Customers are allowed to leave either one suitcase or one bag containing personal belongings. The weight of the bag or suitcase should not exceed 100 pounds, and it must be placed in the trunk or storage area of the shipment. Prior to loading the shipment, the customer is required to inform both Flampeg Solutions LLC and the carrier about any personal property that is included in the shipment at the point of origin. The customer acknowledges and agrees that the carrier reserves the right to refuse any personal property included in the shipment if it poses a safety risk or violates any legal regulations. We advise customers not to include any negotiable instruments, legal papers, jewelry, furs, cash, currency, antiques, or any other valuable items in their shipment. The customer acknowledges and agrees that neither the carrier nor Flampeg Solutions LLC can be held responsible for any personal items of any kind or value left in the shipment or for any damage caused to the shipment due to excessive or improper loading of personal items. If the customer decides to include their personal property in the shipment, they do so at their own risk.</li>
+            <li><b>Prohibited Items.</b>  The customer acknowledges and agrees that they are strictly prohibited from including any explosives, guns, ammunition, weapons, flammable products, live pets, live plants, contraband, drugs or narcotics, alcoholic beverages, or any illegal goods in the shipment. The customer acknowledges and agrees that if any prohibited items are found in their shipment, the carrier or law enforcement may confiscate or dispose of them. In such cases, the order may be canceled without any compensation to the customer. Additionally, the customer will be held responsible for any fees, fines, damages, or liabilities that arise from violating this section.</li>
+            <li><b>Shipment by sea.</b>  The customer acknowledges and agrees that no personal belongings of any kind or value, as well as any illegal items, will be permitted in the shipment for transportation by sea. The customer is responsible for ensuring that the shipment is entirely empty, except for any original equipment manufacturer or factory-installed equipment. The customer is in charge of supplying all necessary paperwork and documents in accordance with both domestic and foreign customs requirements. To place an order, the customer is required to provide the vehicle identification number (VIN) and an estimated value of the vehicle in U.S. dollars. In the event that the point of origin or destination happens to be a shipping port, the customer is responsible for paying any additional fees that may be associated with it.</li>
+        </ul>
+        
+
+
+
+
+        <h2>FEES :</h2>
+        <p>When a client places an order for Flampeg Solutions LLC services, the company has the right to charge a non-refundable booking fee of up to $100. By entering into this agreement, you are acknowledging your responsibility to pay all fees associated with the services provided by Flampeg Solutions LLC, as well as any other charges that may arise. The fees for Flampeg Solutions LLC services will be clearly stated in the order confirmation email and on the Flampeg Solutions LLC website prior to submitting your order. The entire fee for transporting your vehicle will be divided into two installments. The first installment, known as the initial payment, is due when Flampeg Solutions LLC assigns a carrier to transport your vehicle. The second installment, known as the remaining balance, is due when your vehicle is delivered.</p>
+        <p>Payment for the initial amount must be made using a credit or debit card. When you submit an order, please include your credit or debit card information. This will authorize Flampeg Solutions LLC to conduct a $1 authorization to validate the card. Once the initial payment becomes due, the card will be charged.</p>
+        <p>Please note that the remaining balance and any additional charges that may arise as per the terms of this agreement must be paid in full at the time of delivery of your vehicle. You can make the payment either in cash or through a certified check directly to the carrier. If you fail to pay the remaining balance on time, you may have to bear the cost of storing the vehicle until the balance is fully paid.</p>
+        <p>All payments must be made in full by their due date, and the fees cannot be disputed or offset for any reason, including if you are dissatisfied with the services or if there are damages. After payment, the charges cannot be refunded.</p>
+        <p>If you do not make a payment on time or if we receive a chargeback for your payment due to a dispute or any other reason, you are required to pay Flampeg Solutions LLC the outstanding amount owed. Additionally, you must reimburse all reasonable costs incurred in collecting any overdue payment or chargeback, including attorneys' fees, legal costs, court costs, and collection agency fees. Interest will be charged on late payments and chargebacks from the original due date. The interest rate will be the lower of 1% per month or the highest rate allowed by law.</p>
+
+
+
+
+        <h2>Fully Insured :</h2>
+        <p>The carrier is solely responsible for any damage that occurs to a vehicle during transport, not Flampeg Solutions LLC. All contracted carriers will be insured to cover any damages caused by the driver or theft that may occur during transportation. Flampeg Solutions LLC will provide you with a complete insurance packet to file a claim with the carrier in case of any damage.</p>
+        <p>Carriers are not authorized or covered by insurance to transport personal or household items. Nonetheless, we acknowledge that you may need to place some items in the vehicle. Please ensure that these items are placed in the trunk and that their total weight does not exceed 120 pounds. Flampeg Solutions LLC must approve any exceptions after prior discussion. A fee may be charged for personal items, regardless of their weight. In the event of any misrepresentation of personal belongings, there will be a change in price and/or a dry run fee of $130. This fee will be charged if the carrier has to attend the pick-up scene and the shipment is different from what was expected.</p>
+
+
+
+
+        <h2>Vehicle Inspection :</h2>
+        <p>Under no circumstances should the client release or receive vehicle(s) from a carrier without an inspection report (Bill of Lading or BOL), regardless of the time of day or weather conditions. If the client fails to do so, they may not be able to file a damage claim. Carriers insurance will solely handle claims for damages that result from the carrier's negligence. Please ensure that any damages are reported to Flampeg Solutions LLC within 24 hours of delivery. All damages should be clearly listed on the Bill of Lading (BOL) and signed by the driver without any exceptions. If any damage occurs during transportation, it is important for the client to note those damages on the final inspection report. After that, they should pay the remaining balance mentioned in the agreement and get in touch with the carrier's main office and insurance company. If you fail to note any damage on the final inspection report, the carrier will not be held liable, and you will not be able to file a damage claim.</p>
+
+
+
+        <h2>Governing Law :</h2>
+        <p>The client and Flampeg Solutions LLC have both fully participated in reviewing and revising these terms. They have also had the opportunity to seek legal counsel for advice. This agreement shall not be subject to any rule of construction that resolves ambiguities against the drafting party.</p>
+        <p>The laws of the State of Texas shall govern and interpret this agreement. Both parties have mutually agreed that any legal action related to this agreement must be filed in a court of jurisdiction within Texas. It is also agreed that Flampeg Solutions LLC's liability will be limited to the amount of money charged as Flampeg Solutions LLC's fee. The client acknowledges and agrees to the jurisdiction of the aforementioned courts and relinquishes any entitlement to jurisdiction in any other place.</p>
+
+
+
+        <h2>Contact Information :</h2>
+        <p>Questions about the Terms of Service should be sent to us at info@rapidautoshipping.com and the <a href="https://rapidautoshipping.com/contact-us" target="_blank">contact page</a>.</p>
+    </div>
+    <?php include_once page_bottom ?>
+   
+</body>
+</html>
