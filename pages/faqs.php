bf41026519550fda0b595d4a3322732e9bea48a7
Faiz <mohdfaiz.1842@gmail.com>
2023-05-29 12:46:20 +0530

    add faq page

---
+++ pages/faqs.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAQS</title>
+    <?php include head;    ?>
+    <link rel="stylesheet" href="<?= get_css(); ?>faq.css">
+    <link rel="stylesheet" href="<?= get_css(); ?>car-key-section.css">
+</head>
+<body>
+<div class="body">
+        <div class="faq">FACTS & QUESTIONS</div>
+        <div class="container">
+            <div class="box">
+                <span>Is it worthwhile to transport a car?</span>
+                <i class='bx bx-plus'></i>
+            </div>
+            <p class="para-faq">The value of your automobile is one of the most important elements in determining whether it is worthwhile to transport a car. If you're moving across the nation and have an older vehicle, the expense of shipping it may exceed the vehicle's worth.
+                <br><br>
+                Determine whether it is preferable to sell your vehicle and buy a new one at your location. Another option is to drive yourself to your location. Driving can be more affordable if you don't mind putting kilometers on your car.
+            </p>
+            <div class="box">
+                <span>What is the cost of shipping an automobile 2,000 miles?</span>
+                <i class='bx bx-plus'></i>
+            </div>
+            <p class="para-faq">Shipping a car 2,000 miles between metro regions might cost between $1,300 and $1,700. You'll spend extra if you're transporting to or from a remote area, or if an enclosed vehicle transport services is required.
+            </p>
+            <div class="box">
+                <span>What is the typical expense of shipping a car?</span>
+                <i class='bx bx-plus'></i>
+            </div>
+            <p class="para-faq">
+                The typical expense of shipping a car is determined by many variables. Car shipping costs are affected by factors such as vehicle weight, mode of transit, and distance.
+                <br><br>
+                We discovered that the average price for vehicle transportation ranges between $800 and $2,000, but the price fluctuates depending on the auto transport provider.
+            </p>
+            <div class="box">
+                <span>How do I send my vehicle?</span>
+                <i class='bx bx-plus'></i>
+            </div>
+            <p class="para-faq">The first stage in shipping your vehicle is to evaluate car shipment quotes from a few auto transport firms online. You must choose your shipment dates as well as whether you want open or enclosed vehicle transportation as well as door to door or terminal to terminal transport.
+                <br><br>
+                After you've decided on a transporter, the company will manage the arrangements. Simply remove your personal belongings, leave your car off at a designated location, and then collect it up at your destination.
+            </p>
+            <div class="box">
+                <span>What characteristics should I search for in a car delivery service?</span>
+                <i class='bx bx-plus'></i>
+            </div>
+            <p class="para-faq">Low pricing, guaranteed estimates, and insurance coverage choices are all things to look for in a vehicle delivery service.</p>
+            <div class="box">
+                <span>When is the cheapest time of year to send a car?</span>
+                <i class='bx bx-plus'></i>
+            </div>
+            <p class="para-faq">The lowest vehicle shipping costs are often seen in the fall and early spring months. Higher charges result from increased demand for car transport services in January and the summer months.
+            </p>
+            <div class="box">
+                <span>Is it possible to send my automobile with personal goods inside?</span>
+                <i class='bx bx-plus'></i>
+            </div>
+            <p class="para-faq">Personal things cannot be shipped in your vehicle with most car transport services. Some may enable you to load a suitcase or box into the trunk.
+                <br><br>
+                However, no carrier's insurance coverage will cover goods inside your vehicle that are damaged during shipment, therefore we recommend that you thoroughly clean your vehicle. 
+               
+            </p>
+            <div class="box">
+                <span>Is insurance required when shipping a car?</span>
+                <i class='bx bx-plus'></i>
+            </div>
+            <p class="para-faq">Vehicle insurance is not required while moving an automobile. It is the delivery company's responsibility to get and maintain liability insurance. You can, however, choose to buy additional damage protection if you so desire.
+            </p>
+            <div class="box">
+                <span>Reliable antique car transport service</span>
+                <i class='bx bx-plus'></i>
+            </div>
+            <p class="para-faq">Classic automobiles are investments! Time is money, but the passion and work put into these automobiles are incomparable.
+                <br><br>
+                Rapid Auto Shipping engages with you and knows your degree of appreciation, so our antique car transport services mirror your desire to safeguard your investment! With enclosed luxury car carriers, you now have a dependable luxury car transport service!
+            </p>
+            <div class="box">
+                <span>How does car transportation insurance work?</span>
+                <i class='bx bx-plus'></i>
+            </div>
+            <p class="para-faq">The carrier must have auto transport insurance that covers your vehicle (but not the contents) during loading, transit, and unloading.
+                <br><br>
+                If necessary, you may also acquire supplementary insurance. You do not need your auto insurance to send your car, but you will need it to drive it away and to submit a claim if necessary.
+            </p>
+            
+        </div>
+    </div>
+    <script src="..//assets/js/faq.js"></script>
+</body>
+</html>
