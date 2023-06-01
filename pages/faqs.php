fb319ebba85368bbc9d5696e3054eb6f105d7002
Faiz <mohdfaiz.1842@gmail.com>
2023-06-01 15:42:58 +0530

    add

---
+++ pages/faqs.php
@@ -0,0 +1,241 @@
+<?php include_once "../init.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terms & Conditions</title>
+    <meta property="og:description" content="terms and conditions about rapid auto shipping" />
+    <?php include head;    ?>
+    <link rel="stylesheet" href="<?= get_css(); ?>car-key-section.css">
+    <style>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+  body{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+.faqs{
+    padding: 50px;
+    background-color: #80808021;
+    font-family: "Poppins", sans-serif;
+}
+.faqs h1{
+    text-align: center;
+    margin-bottom: 15px;
+}
+.faqs h2{
+    margin-top: 20px;
+}
+.faqs ul span{
+    font-weight: bold;
+}
+.faqs p{
+    font-weight: 400;
+}
+    </style>
+</head>
+<body>
+<?php include header; ?>
+    <div class="faqs">
+
+
+        <h1>Frequently Asked Questions</h1>
+        
+
+        <h2>How Long Will It Take to Ship My Car?</h2>
+        <p>The typical time to ship an automobile inside the continental United States is one to two weeks. However, shipping a car to Hawaii or Alaska may take up to eight weeks or more. In comparison, these are broad ranges; factors like transport truck bandwidth, traffic, weather, and other factors will influence your specific timings.</p>
+
+
+
+        <h2>How Much Should I Expect to Spend on Shipping My Car?</h2>
+        <p>Vehicle freight typically ranges from 40 cents to $2.00 per mile. Compared to long-distance and cross-country shipping, short-distance moves utilising open transit have lower total costs but higher average expenses per mile. You might obtain lower charges if you are more flexible with your drop-off and pickup dates.</p>
+        
+
+
+
+        <h2>What Kind of Paperwork Will I Need?</h2>
+        <p>Here are a few important documents that car transport companies may require to ship your vehicle:</p>
+        <ul>
+            <li><span>1. </span> Bill of lading: The bill details the things being sent, the shipper and recipient's addresses, and other crucial cargo information.</li>
+            <li><span>2. </span>Insurance Conditions of Use: This document describes the coverage provided by a shipping company's insurance policy.</li>
+            <li><span>3. </span>Lienholder authorization: In case of an accident or other mishap during shipment, this agreement enables the shipping firm to act on behalf of the lienholder (the entity with a financial interest in the car).</li>
+            <li><span>4. </span>Absent owner authorization: The shipping business is authorized to operate on behalf of the absent owner under this agreement.</li>
+        </ul>
+
+
+
+
+        <h2>Who Will Be Transporting My Car?</h2>
+        <p>The auto transport company provides various automobile shipping options, including domestic shipping with trailer trucks and international shipment using cargo ships or aeroplanes. Nonetheless, using a trailer truck is the most common method of vehicle shipping in the US. Open transport and enclosed transport are the two categories of land-based vehicle transportation.</p>
+        
+        
+
+
+
+
+        <h2>Where Will My Car Be Picked Up/Dropped Off?</h2>
+        <p>Vehicle transfer companies may need help to provide accurate transportation and drop-off times. Unforeseen obstacles, such as traffic jams or mechanical breakdowns, can result in delays. Patience, preparation, and communication with your vehicle transporter will all contribute to a positive experience for both parties. <br><br>
+            You can set up a rendezvous location that you and your carrier can reach easily. If the streets in your area are challenging to navigate with a vehicle, you may need to meet in a nearby parking lot. If you cannot give the car to the driver in person, assign duty to a trustworthy representative.</p>
+
+
+
+
+        <h2>Should I Book Car Transportation Service Near Me?</h2>
+        <p>When you are migrating to a new location, the first thing that springs to mind is the car transportation service near me the nation. No, driving yourself there is not a good idea for you or your gorgeous automobile. Instead, please take advantage of the convenience of vehicle transport near me and have your car moved to your new house without breaking a sweat. Another advantage of hiring auto transporters nationwide is that you will not need to hunt for packers and movers. The greatest internet directories in the United States allow you to contact the best auto carrier in the country.</p>
+
+
+
+
+        <h2>What Kind of Tracking Services Do You Offer?</h2>
+        <p>You may access online monitoring at any time of day or night, which is always up to date. When you plan a shipment, you will be issued a unique booking number that will allow you to track the progress of your cargo online, and you may monitor your vehicle by phone, email, or Live Chat.</p>
+
+
+
+        <h2>How Will I Know When My Car is Being Delivered?</h2>
+        <p>The carrier will contact you when they are only a few hours away from the delivery location. This is just a friendly reminder. They are, however, telling you so that you may make sure you are accessible to accept your automobile. So don't be concerned if you know you'll be busy; other arrangements can be made.</p>
+
+
+
+        <h2>My Car Still Hasn’t Arrived. Should I Be Worried?</h2>
+        <p>Most auto shippers do their utmost to fulfill delivery expectations, but delays are a daily reality within the vehicle moving industry. But you don't have to worry about the delay; you can always contact a car transporter and auto shipping company's staff and track your shipment online and by phone, email, or Live Chat.</p>
+
+
+
+        <h2>Will My Exotic/Luxury Car Be Safe?</h2>
+        <p>When transporting exotic vehicles, the driver must guarantee that each one is secure during transportation. Drivers are taught professional strategies for loading low-profile automobiles with high horsepower onto specialist carriers. They know how to secure them without causing harm and discharge them securely for delivery. There is no haste in the procedure, and no detail is neglected.</p>
+
+
+
+        <h2>How Do I Pay for Shipping My Car?</h2>
+        <p>We accept bank/certified checks, all major credit cards, electronic transfers, postal money orders, and other reviews for the deposit or full pre-payment of your cargo. A balance owed at the delivery time can be paid in cash, bank/certified check, or postal money order made payable to the driver.</p>
+
+
+
+        <h2>Do You Ship Cars Internationally?</h2>
+        <p>The general procedure is the same with international automobile exports, although there is more documentation. You'll often be requested to supply the documentation because you'll be exporting to a nation with different laws.</p>
+
+
+
+        <h2>Should I Expect Any Extra Charges Once My Car is Delivered?</h2>
+        <p>The distance you send your vehicle will greatly influence the price. However, you may be charged additional costs if you are shipping your vehicle from or to a remote place. For example, auto carriers occasionally charge extra fees to drive your vehicle to a less inhabited area because it takes more time and is usually much farther from the carrier's following location.</p>
+
+
+
+        <h2>Will You Pick Up Cars that Don’t Run?</h2>
+        <p>Every day, we transport non-operational vehicles. To load your car, we will arrange a truck with a winch. It may take longer and cost more to transport disabled vehicles because not all truckers can do so on their routes.</p>
+
+
+
+        <h2>Is My Pick-Up or Delivery Date Guaranteed?</h2>
+        <p>Ensure pickup dates. We certainly can! All you have to do is specify whether you need the car picked up by a selected date or on a specific day, which will determine the additional fee associated with ensuring your time frame. If you want a definite pickup date(s), please get in touch with us for tailored quotes. The auto-hauling sector, however, does not guarantee delivery dates because things can happen on the road.</p>
+
+
+
+        <h2>Can I transport my personal belongings in the car?</h2>
+        <p>In most cases, you can store up to 100 pounds of personal items in the trunk or storage section of your vehicle, truck, or SUV. Otherwise, your car must be vacant. This is because those auto transporters do not have a license or insurance to move household goods across state borders.</p>
+
+
+
+        <h2>How can I cut costs on car shipping?</h2>
+        <p>There are several ways to save money on car shipping, including using open transport rather than enclosed transport and shipping from terminal to terminal. You can also save money on vehicle shipping by selecting flexible arrival dates.</p>
+
+
+
+        <h2>What is the cheapest car shipping service?</h2>
+        <p>Open vehicle shipping is the cheapest method to send your automobile. The only disadvantage of this choice is that your car will be exposed to the elements while being transported.</p>
+
+
+
+        <h2>What kinds of cars can be transported?</h2>
+        <p>Auto transporters can usually carry any vehicle to your transportation location, including sedans, SUVs, premium cars, and motorbikes. Automobiles that are not operable or are in poor shape can also be transported for a charge.</p>
+
+
+
+        <h2>What Is The Expense Of Car Shipping 1,000 Miles?</h2>
+        <p>Depending on the provider, the usual cost for a 1,000-mile shipping task could vary from $750 to $1,000 or more. Other variables to consider include the time of year, the sort of vehicle being transported, and the mode of transportation.</p>
+
+
+
+        <h2>Do I need to be present when your vehicle is picked up or dropped off?</h2>
+        <p>Yes, we require you or a designated representative to be present when picking up your vehicle. You must execute a contract known as the "bill of loading" once the car has been loaded for shipment. If you cannot be present, don't hesitate to contact our office before the scheduled pick-up date to confirm a replacement driver. However, alterations to the pick-up or delivery address after the order has been submitted may incur additional fees.</p>
+
+
+
+        <h2>Can I leave or collect my vehicle at a terminal?</h2>
+        <p>Yes, we have Transport Terminals in the majority of significant U.S. cities. This is an excellent option for anyone seeking immediate vehicle delivery.</p>
+
+
+
+        <h2>How do I prepare my vehicle for shipping?</h2>
+        <p>You can take several measures to prepare your vehicle for auto transport:</p>
+        <ul>
+            <li><span>1. </span>Remove all personal items from the vehicle, including aftermarket electronics or accessories.</li>
+            <li><span>2. </span>Ensure that the car is spotless inside and out. This will facilitate the transport company's vehicle examination for preexisting damage.</li>
+            <li><span>3. </span>Check the fluid levels in the vehicle and inflate the tires to the correct pressure.</li>
+            <li><span>4. </span>Ensure the vehicle is in excellent functional condition and has no fluid leaks.</li>
+            <li><span>5. </span>If present, disable the car's alarm system.</li>
+            <li><span>6. </span>Keep approximately a quarter tank of petroleum in the vehicle.</li>
+            <li><span>7. </span>Before transporting the vehicle, photograph it from all angles to document its condition.</li>
+            <li><span>8. </span>Ensure you have all required documentation, including the car's title and registration.</li>
+            <li><span>9. </span>Notify the conveyance company in advance if the vehicle is inoperable so that they can make the necessary arrangements.</li>
+        </ul>
+        <p>If you transport your vehicle to Alaska or Hawaii with more than a quarter petroleum tank, you will be charged an additional de-gassing fee.</p>
+
+
+
+        <h2>Does Rapid Auto Transport maintain insurance?</h2>
+        <p>Yes, the cargo insurance policy of Rapid Auto Shipping covers your vehicle for its total replacement value while in our possession.</p>
+
+
+
+        <h2>How do I obtain a quote for transporting my vehicle?</h2>
+        <p>To receive a quote for transporting your vehicle, use our instant online car shipping quote calculator above this page or call +1 (833) 233-4447 to speak with one of our car transport pricing specialists. To compute your estimate, we need to know the origin and destination locations, the type of vehicle you wish to ship, and the date your car will be available.</p>
+
+
+        <h2>Can I transport an altered or oversized vehicle?</h2>
+        <p>We can dispatch vehicles of any capacity thanks to our advanced national network of specialized carriers. However, you must call for a customized rate quotation based on the dimensions of the oversize vehicle you wish to transport. Our online quotation calculator cannot provide you with a quote because the calculator's pricing is based solely on standard and unmodified vehicle measurements and specifications.</p>
+
+
+
+        <h2>Can I ship a low-ground-clearance vehicle?</h2>
+        <p>Yes, but any vehicle with a ground clearance of less than 4 inches will likely require an enclosed trailer with a lift gate. Low-clearance vehicles cannot be transported onto an uncovered trailer because the car would be at risk of bottoming out during loading.</p>
+
+
+
+        <h2>What is the cancellation penalty?</h2>
+        <p>Nothing There will be no cancellation fees, gasoline surcharges, or other needless cleansing of the human soul.</p>
+
+
+        <h2>What if the truck is too huge to fit my drop-off or pick-up zone?</h2>
+        <p>Most vehicle transport trucks are huge, measuring up to 75 feet long. This makes it difficult to reach many residential areas at times.</p>
+        <p>If this is the case with your auto relocation, your carrier will try to get as near your delivery address as feasible. We may also arrange for a suitable location, such as a shopping mall, to pick up or deliver your vehicle.</p>
+        <p>In certain situations, we may be able to negotiate a lower shipping fee for you if you are prepared to meet your driver at a more accessible, convenient location that works for both of you.</p>
+        <p>They will not relocate your automobile; instead, they will put you in touch with firms that will (such as us).</p>
+
+
+
+        <h2>Is door to door shipment preferable to terminal to terminal shipping?</h2>
+        <p>A: Aside from door-to-door car transport, another alternative is terminal-to-terminal auto transport. However, there are better ways than this because there is no insurance coverage at these facilities, and your car changes hands many times. More clients are choosing our "door to door" service for convenience and peace of mind. <br>
+            If you are considering transporting your automobile, we propose our door-to-door auto transport services since they are the quickest and most convenient.</p>
+
+
+
+        <h2>How does the automobile shipping procedure work?</h2>
+        <p>Once you complete your purchase online (very safe) or over the phone, your vehicle will be shipped on the first available day of your shipping order. Ninety percent of our orders are assigned within one week of the day they are placed. Once allocated, the car is normally picked up within a day or two of the ship date. </p>
+        <p>When it is assigned, we instantly email the customer, and the automobile shipment driver contacts your pickup contact. When the driver arrives to pick up your vehicle for auto transport, he will thoroughly check it on the Bill of Lading. You will both sign and receive a copy of this form. As he approaches the location, the automobile shipment driver will phone your target contact from the road. The same Bill of Lading will be present at delivery, allowing you to check the car.</p>
+
+
+
+
+        <h2>Is Rapid Auto Shipping Reliable Transportation Company?</h2>
+        <p>Rapid Auto Shipping can help you to transport your vehicle! We can transport your vehicle to any United States and overseas location. Our representatives are standing by to answer your inquiries and assist you in scheduling car transport services immediately. For complimentary estimates, please contact us at +1 (833) 233-4447. You can use our online quotation tool to generate a cost if you prefer another way. It is simple to request a free, immediate estimate. There are only four necessary fields: car makes, model, pickup place, and delivery address. There is no need for any confidential details. We eagerly await your response.</p>
+
+
+
+    </div>
+    <?php include_once page_bottom ?>
+   
+</body>
+</html>
