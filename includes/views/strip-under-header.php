bc8d7e088b2b8d61cb39f49da53d3f1f5c967c20
Faiz <mohdfaiz.1842@gmail.com>
2023-06-05 14:07:30 +0530

    add

--- includes/views/strip-under-header.php
+++ includes/views/strip-under-header.php
@@ -6,7 +6,7 @@
             <p>Successful Delivery</p>
         </div>
         <div class="review-bar-under-header-grid-1">
-            <img loading="lazy" src="<?= get_img() ?>globe-icon1-min.png" alt="review rapid auto shipping" />
+            <img loading="lazy" src="<?= get_img() ?>globe-icon1.png" alt="review rapid auto shipping" />
 
             <h2>190+</h2>
             <p>Countries Served</p>
