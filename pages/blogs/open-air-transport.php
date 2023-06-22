6f5b53d58a040b0c2a49a45ae4036206178f7321
Ashutosh Tiwari <ashutoshtiwari9453@gmail.com>
2023-06-22 11:53:39 +0530

    add new page

---
+++ pages/blogs/open-air-transport.php
@@ -0,0 +1,215 @@
+<?php include_once "../init.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Open Air Transport | Rapid auto shipping </title>
+
+    <meta name="title" content="Open Air Transport | Rapid auto shipping | +1-833-233-4447" />
+<meta name="description" content="The most popular and cost-effective method of transporting a car from one location to another is through open air transport car shipping." />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Open Air Transport | Rapid auto shipping | +1-833-233-4447" />
+<meta property="og:description" content="The most popular and cost-effective method of transporting a car from one location to another is through open air transport car shipping." />
+<meta property="og:url" content="https://rapidautoshipping.com/blogs/open-air-transport" />
+<meta property="og:site_name" content="Open Air Transport | Rapid auto shipping | +1-833-233-4447" />
+<meta name='robots' content='index' />
+<meta name="keywords" content="Open Air Transport" />
+<meta property="article:tag" content="Open Air Transport" />
+
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "name": "Rapid Auto Shipping",
+  "url": "https://www.rapidautoshipping.com/",
+  "@id": "https://rapidautoshipping.com/#website",
+  "potentialAction": {
+    "@type": "SearchAction",
+    "target": "https://www.rapidautoshipping.com/search?keywords={search_term_string}",
+    "query-input": "required name=search_term_string"
+  }
+}
+</script>
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "@id": "https://www.rapidautoshipping.com/#organization",
+  "name": "Rapid Auto Shipping",
+  "legalName": "Rapid Auto Shipping",
+  "alternateName": "Rapid Auto Shipping",
+  "url": "https://www.rapidautoshipping.com/",
+  "founder": "Johny Taylor",
+  "foundingDate": "2016",
+  "sameAs": [
+    "https://www.rapidautoshipping.com",
+    "https://twitter.com/ShippingRapid",
+    "https://www.facebook.com/Rapidautoshipping",
+    "https://www.youtube.com/@rapidautoshipping7218",
+    "https://www.instagram.com/rapidautoshipping/",
+    "https://www.linkedin.com/company/rapid-auto-shipping",
+    "https://www.trustpilot.com/review/rapidautoshipping.com",
+    "https://www.transportreviews.com/Company/Rapid-Auto-Shipping",
+    "https://www.provenexpert.com/en-us/rapid-auto-shipping"
+  ],
+  "description": "The most popular and cost-effective method of transporting a car from one location to another is through open air transport car shipping.",
+  "logo": {
+    "@type": "ImageObject",
+    "url": "https://rapidautoshipping.com/assets/images/Untitled-1-Recovered.png"
+  }
+}
+</script>
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Product",
+  "@id": "https://www.rapidautoshipping.com/#product",
+  "name": "Open Air Transport  | Rapid Auto Shipping",
+  "alternateName": "Open Air Transport ",
+  "description": "The most popular and cost-effective method of transporting a car from one location to another is through open air transport car shipping.",
+  "brand": {
+    "@type": "Organization",
+    "@id": "https://www.rapidautoshipping.com/#organization"
+  },
+  "mainEntityOfPage": {
+    "@type": "WebPage",
+    "@id": "https://www.rapidautoshipping.com/#webpage"
+  },
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "@id": "https://rapidautoshipping.com/#aggregateRating",
+    "ratingValue": 4.8,
+    "ratingCount": 545,
+    "worstRating": 1,
+    "bestRating": 5
+  }
+}
+</script>
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Service",
+  "serviceType": "Open Air Transport | Rapid Auto Shipping",
+  "alternateName": "Open Air Transport "
+}
+</script>
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": [
+    {
+      "@type": "ListItem",
+      "position": 1,
+      "name": "Services",
+      "item": "https://www.rapidautoshipping.com/services/"
+    }
+  ]
+}
+</script>
+
+</head>
+<body>   
+   <h1>Open Air Transport</h1>
+   <span style="font-weight: 400;">Our open car transport service is the most popular and cost-effective option for shipping your vehicle. Open carrier car shipping involves transporting your vehicle on an open trailer car carrier, which is frequently seen on highways and used to deliver vehicles to dealerships throughout the country. This method of transportation provides door-to-door service for your car.</span>
+<h2><b>What Is Open Air Transport Car Shipping?</b></h2>
+<span style="font-weight: 400;">The most popular and cost-effective method of transporting a car from one location to another is through open air transport car shipping. </span><span style="font-weight: 400;">The open trailer method is a convenient option for individuals who own multiple cars, dealerships, or manufacturers.</span>
+<br><br>
+<span style="font-weight: 400;"> This method involves transporting several vehicles at once using an open trailer. There are both advantages and disadvantages associated with this method of transporting a vehicle.</span>
+<br><br>
+<span style="font-weight: 400;">If you need to transport cars over long distances, ranging from a few hundred miles to thousands of miles across the country, <a href="https://rapidautoshipping.com/services/open-car-shipping" target="_blank"><strong>open transport car shipping</strong></a> is an ideal option. </span>
+<br><br>
+<span style="font-weight: 400;">This transportation option is comparatively more affordable than others as it involves transporting your car in an open trailer along with several other vehicles, thereby reducing the overall cost. </span>
+<br><br>
+<span style="font-weight: 400;">Moreover, it is a highly flexible option that provides a broader selection of pick-up and delivery times to accommodate your schedule and requirements.</span>
+<br><br>
+<span style="font-weight: 400;">You may see some differences between open air transport below.</span>
+<h3><b>One vehicle</b></h3>
+<span style="font-weight: 400;">The term "single-car open air transport" refers to a trailer that has space for only one vehicle when fully extended. During the entirety of the move, your car will be restrained and left alone on the trailer in its designated location.</span>
+<br><br>
+<span style="font-weight: 400;"> Because this mode of transportation eliminates the need for you to wait for the delivery of other automobiles or worry about other vehicles, you won't have to wait. </span>
+<br><br>
+<span style="font-weight: 400;">Despite this, it is still quite pricey because you have to pay for the entire trip instead of sharing the cost with other customers. This prevents you from sharing the cost with other customers.</span>
+<h3><b>Only one level</b></h3>
+<span style="font-weight: 400;">Your automobile will be traveling alongside a small number of other vehicles because a single-level open carrier may accommodate many cars.</span>
+<br><br>
+<span style="font-weight: 400;"> You don't have to interact with other customers, and it's still a really rapid way. Compared to single-car open air transportation, it is more affordable and accessible.</span>
+<h3><b>Multi-Level</b></h3>
+<span style="font-weight: 400;">Because they can carry most automobiles in a single journey, multiple-level open carriers are frequently used for vehicle transportation. </span>
+<br><br>
+<span style="font-weight: 400;">They have the most affordable prices and are simple to schedule, but they transport several automobiles every trip, which might cause the journey to be delayed.</span>
+<h2><b>Benefits of Shipping Cars in Open Air Transport </b></h2>
+<span style="font-weight: 400;">There are two primary ways to send an automobile: open air transport car shipping and enclosed transport <a href="https://rapidautoshipping.com/cars" target="_blank"><strong>car shipping</strong></a>. Open air transport is still the most common option for a variety of reasons, even if enclosed transport gives better safety for your car.</span>
+<h3><b> Insurance Protection</b></h3>
+<span style="font-weight: 400;">You may rest easy knowing that the transporter will be responsible for any damage to your automobile that occurs during transportation because all carriers have insurance. </span>
+<br><br>
+<span style="font-weight: 400;">Going with open air transport has the added benefit that the driver will be able to spot any damage straight away and will inform you of it right away.</span>
+<h3><b> Accessibility</b></h3>
+<span style="font-weight: 400;">The accessibility of open air transportation is another benefit. You are more likely to discover an open air transport carrier available when needed since they are more prevalent than covered carriers.</span>
+<br><br>
+<span style="font-weight: 400;"> This may be especially helpful if you need to transport your automobile quickly or during a busy time of year, like the holidays or when moving is at its busiest.</span>
+<h3><b> Adaptability</b></h3>
+<span style="font-weight: 400;">Car transportation with open air transport provides more freedom than shipping with <a href="https://rapidautoshipping.com/services/enclosed-shipping" target="_blank" ><strong>enclosed transport</strong></a>. For instance, open air transport can be your only choice if you deliver a massive automobile that won't fit in an enclosed trailer.</span>
+<br><br>
+<span style="font-weight: 400;"> You may transfer all of your cars at once thanks to open air transport carriers' capacity for numerous vehicles of various brands and models.</span>
+<h3><b> Increased Exposure</b></h3>
+<span style="font-weight: 400;">Finally, increased visibility is a benefit of open-transport automobile shipment that may benefit both you and the transport company. </span>
+<br><br>
+<span style="font-weight: 400;">Carriers can check automobiles more efficiently throughout the transportation process since open air transport trailers are exposed to the weather. Additionally, your car is less likely to disappear or be stolen because it is clearly visible to all onlookers.</span>
+<h2><b>Cost to Ship A Car In Open Air Transport</b></h2>
+<span style="font-weight: 400;">Open vehicle transport has relatively reasonable rates for shipping automobiles. You may anticipate estimates to range from around $500 for a brief interstate journey to $1,500 for <a href="https://rapidautoshipping.com/blogs/cross-country-car-shipping" target="_blank"><strong>cross-country transportation</strong></a>, although there are a number of considerations at play.</span>
+<h3><b>Distance</b></h3>
+<span style="font-weight: 400;">For the first 1 to 500 miles of open-air vehicle transportation, the standard fee is $1 per mile. The cost per mile decreases as you travel deeper. </span>
+<br><br>
+<span style="font-weight: 400;">For instance, transferring an automobile 1,000 miles reduces the price to $.75 per mile, assuming no other cost-related considerations. Depending on the route, the cost of coast-to-coast vehicle shipping can be as low as.40 cents per mile.</span>
+<h3><b>Points of Pick-up and Dropoff</b></h3>
+<span style="font-weight: 400;">Urban regions often have lower shipping prices since there are more transport trucks available to accommodate consumer schedules. </span>
+<br><br>
+<span style="font-weight: 400;">However, for lower prices and quicker delivery, if you live distant from a large urban region, you can opt to meet the driver there.</span>
+<h3><b>Size and weight of the vehicle</b></h3>
+<span style="font-weight: 400;">The cost of delivering your car will depend on its dimensions and weight. For instance, shipping a four-door vehicle from the northeast to <a href="https://rapidautoshipping.com/states/florida" target="_blank"><strong>Florida</strong> </a>can run you between $650 and 850.</span>
+<br><br>
+<span style="font-weight: 400;"> For the same route, the same operator may charge between $900 and $1100 to move SUVs, vans, or pick-up trucks.</span>
+<br><br>
+<span style="font-weight: 400;"> The number of cars that can fit on a carrier is impacted by larger vehicles taking up more space. The carrier is loaded down by heavier cars, which increases fuel expenses.</span>
+<h3><strong>Period of Time</strong></h3>
+<span style="font-weight: 400;">Due to the high demand for vehicle transport to warmer regions in the winter, it could be more difficult to locate empty space on a carrier at a reasonable price. </span>
+<br><br>
+<span style="font-weight: 400;">You might potentially save hundreds of dollars on your vehicle delivery if you can wait to make the same journey during the off-peak season.</span>
+<h2><b>What You Should Know Before Booking?</b></h2>
+<span style="font-weight: 400;">Your car has to be able to move. If not, extra <a href="https://rapidautoshipping.com/blogs/car-shipping-cost" target="_blank"><strong>costs</strong> </a>or payment options could be necessary.</span>
+<ul>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">Payment is received when the carrier is allocated the vehicle. You must specify when you book if the car is being picked up or delivered at a company. Give the name of your company, a phone number, and the days and times you are available to accommodate.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">Please give us a functional phone number where we may reach you during the entire process of moving your vehicle.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">The handling of a pre-1980 vehicle may call for additional care. Consult your salesperson.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">To load and unload your car securely, it must not be taller than 7' and have a minimum of 4" of ground clearance.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">There must be no evident fluid leaks in the vehicle.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">The door and window on the driver's side must operate correctly.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">Your car's brakes must function correctly. Your vehicle must have a functional emergency brake with a manual transmission.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">Any changes to your vehicle, such as a 4x4 lift kit, camper shell, decreased vehicle clearance, etc., must be disclosed to us.</span></li>
+</ul>
+<h2><b>Preparing Your Vehicle for Transportation:</b></h2>
+<ul>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">In order to pass inspection, the car needs to be spotless. The capacity to carry out a quality check is hindered by a vehicle that is unclean.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">The gasoline level in the gas tank must be at least equal to one-quarter of its capacity, but it cannot exceed one-half of its capacity.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">It is necessary to disconnect, deactivate, or switch off each and every alarm system.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">In addition to car phones, garage door openers, and E-Z toll tag passes, the following equipment needs to be removed from the vehicle: radios, tape decks, and CD players that are not built in; radios, cassette decks, and CD players that are not built in.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">It is required that all antennas be completely retracted or uninstalled.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">It is required that any external coverings, such as spare tire covers, grill covers, or automobile covers, be removed.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">Transporting illegal items, including guns, plants, and potentially harmful materials, is against the law.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">It is required that one complete set of ALL the vehicle's keys be given.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">Non-permanent baggage racks, bike racks, or ski racks must be removed.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">Your car has to be adjusted for the new weather conditions. This may include the coolant for the engine, the oil for the gearbox, and any other fluids.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">It is required that all personal possessions be removed. </span></li>
+</ul>
+<h2><b>Pre- And Post-Move Inspection Requirements For Motor Vehicles Are As follows:</b></h2>
+<ul>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">You or a representative that you have selected must be present throughout the inspection, both when the item is being picked up and when it is being delivered.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">Confirming the <a href="https://rapidautoshipping.com/states" target="_blank"><strong>state</strong> </a>of your car is your responsibility, either personally or by agents you have authorized. This is something that needs to be taken care of before the driver departs the location.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">At both the origin and the destination, the bill of lading and the condition report need to be signed by either you or a representative that you have selected.</span></li>
+ 	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400;">On the bill of lading, any exceptions regarding the condition of the vehicle must be stated.</span></li>
+</ul>
+</body>
+</html>
