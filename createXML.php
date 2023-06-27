72a54ec1077fb059e0f05e241b4cc0bb1ec1a4e1
deepak <deepakbaradwaj933@gmail.com>
2023-06-27 16:21:11 +0530

    sitemap

--- createXML.php
+++ createXML.php
@@ -7,7 +7,6 @@ $dbName = 'u841070527_autoshipping';
 $dbUser = 'u841070527_rapid';
 $dbPass = 'Smile@1427';
 
-// Create a new MySQLi connection
 $mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
 
 // Check if the connection was successful
@@ -23,14 +22,26 @@ $pages = array(
     'blogs',
     'cars',
     'states',
-    'state_to_state',
+    'state-to-state',
+    'auto-transport-carriers',
+    'about-us',
+    'contact-us',
+);
+$pagess = array(
+    'services',
+    'blogs',
+    'cars',
+    'states',
+    'state-to-state',
+    'auto-transport-carriers',
+    'city',
 );
 $xml = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
 $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
 $url = htmlspecialchars('https://rapidautoshipping.com/');
 $xml .= "\t<url>\n";
 $xml .= "\t\t<loc>$url</loc>\n";
-$xml .= "\t\t<lastmod>"."2023-06-20 12:56:17"."</lastmod>\n";
+$xml .= "\t\t<lastmod>"."2023-06-22T13:23:31+05:30"."</lastmod>\n";
 $xml .= "\t\t<changefreq>weekly</changefreq>\n";
 $xml .= "\t\t<priority>1.00</priority>\n";
 $xml .= "\t</url>\n";
@@ -38,26 +49,45 @@ foreach($pages as $page){
         $url = htmlspecialchars('https://rapidautoshipping.com/'.$page);
         $xml .= "\t<url>\n";
         $xml .= "\t\t<loc>$url</loc>\n";
-        $xml .= "\t\t<lastmod>"."2023-06-22 8:30:17"."</lastmod>\n";
+        $xml .= "\t\t<lastmod>"."2023-06-23T16:29:31+05:30"."</lastmod>\n";
         $xml .= "\t\t<changefreq>weekly</changefreq>\n";
         $xml .= "\t\t<priority>0.8</priority>\n";
         $xml .= "\t</url>\n";
 }
-foreach($pages as $page){
-    $query = "SELECT * FROM  $page";
+foreach($pagess as $page){
+    if($page == 'city'){
+        $query = "SELECT * FROM  city ";
+    }elseif($page == 'state-to-state'){
+        $query = "SELECT * FROM  state_to_state ";
+    }elseif($page == 'auto-transport-carriers'){
+        $query = "SELECT * FROM  company_reviews ";
+    }
+    else{
+        $query = "SELECT * FROM  $page";
+    }
     $result = $mysqli->query($query);
     // Create the sitemap XML
-   
-
-    while ($row = $result->fetch_assoc()) {
-        $url = htmlspecialchars('https://rapidautoshipping.com/'.$page.'/'.$row['slug']);
-        $xml .= "\t<url>\n";
-        $xml .= "\t\t<loc>$url</loc>\n";
-        $xml .= "\t\t<lastmod>" . $row['added_on'] . "</lastmod>\n";
-        $xml .= "\t\t<changefreq>weekly</changefreq>\n";
-        $xml .= "\t\t<priority>0.8</priority>\n";
-        $xml .= "\t</url>\n";
-    }
+    if(mysqli_num_rows($result)>0){
+        while ($row = $result->fetch_assoc()) {
+            if($page == 'city'){
+                $url = htmlspecialchars('https://rapidautoshipping.com/'.$row['slug']);
+                $xml .= "\t<url>\n";
+                $xml .= "\t\t<loc>$url</loc>\n";
+                $xml .= "\t\t<lastmod>" . $row['added_on'] . "</lastmod>\n";
+                $xml .= "\t\t<changefreq>weekly</changefreq>\n";
+                $xml .= "\t\t<priority>0.8</priority>\n";
+                $xml .= "\t</url>\n";
+            }else{
+                $url = htmlspecialchars('https://rapidautoshipping.com/'.$page.'/'.$row['slug']);
+                $xml .= "\t<url>\n";
+                $xml .= "\t\t<loc>$url</loc>\n";
+                $xml .= "\t\t<lastmod>" . $row['added_on'] . "</lastmod>\n";
+                $xml .= "\t\t<changefreq>weekly</changefreq>\n";
+                $xml .= "\t\t<priority>0.8</priority>\n";
+                $xml .= "\t</url>\n";
+            }
+        }
+    }else{echo 'Not Found';}
 
    
 }
@@ -71,4 +101,6 @@ file_put_contents($sitemapPath, $xml);
 // Close the database connection
 $mysqli->close();
 
+// Output a success message
+echo "Sitemap generated successfully!";
 ?>
