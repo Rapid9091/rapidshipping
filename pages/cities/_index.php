836f3ceb8c42968565c862a3b76aa753c0aff750
deepak <deepakbaradwaj933@gmail.com>
2023-06-30 14:45:09 +0530

    city bg image updated

--- pages/cities/_index.php
+++ pages/cities/_index.php
@@ -129,7 +129,7 @@ if (isset($_GET['page_id'])) {
         </div>
         <!-- End Header -->
         <div class="banner-section" id="myDiv2">
-            <img class="banner-section-img" id="desk_image" width="100%" height="100%" src="<?= get_img() ?>car-shipping-header-image1.webp" alt="Rapid Auto Shipping">
+            <img class="banner-section-img" id="desk_image" width="100%" height="100%" src="<?= get_img() ?>city_desktop_img.webp" alt="Rapid Auto Shipping">
             <div class="row banner-section-row p-5">
                 <div class="col-md-12 col-lg-8 p-4 banner-section-row-col-1">
                     <div class="row p-2">
